33054aca11fee8510e5cccbb9032052f29e486ff
Steffi7182 <steffi12ammu@gmail.com>
2024-10-24 12:22:57 +0530

    new commit added

--- Grid/app/src/main/java/com/example/grid/MainActivity.java
+++ Grid/app/src/main/java/com/example/grid/MainActivity.java
@@ -1,28 +1,21 @@
 package com.example.grid;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+.
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
 }
-design a login page using linear layout?
-3 fieds : width (full)
-         height (full)
-         orientation(vertical)
+
+
+
+
